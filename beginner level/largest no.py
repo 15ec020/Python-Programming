c994c628e53553ece3b84e61a0d73123e91c4d00
15ec020 <karshinisekar1998@gmail.com>
2018-02-23 12:00:51 +0530

    Create largest no.py

---
+++ beginner level/largest no.py
@@ -0,0 +1,11 @@
+#python code goes here
+#python version :3 
+a=int(input())
+b=int(input())
+c=int(input())
+if(a>b and a>c):
+ print("a is greater")
+elif(b>a and b>c):
+ print("b is greater")
+else:
+  print("c is greater")
