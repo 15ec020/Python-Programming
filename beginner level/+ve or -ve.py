91bf959430ca0d073252c084a098bfb4e8b6e6f2
15ec020 <karshinisekar1998@gmail.com>
2018-02-23 09:19:43 +0530

    Create +ve or -ve.py

---
+++ beginner level/+ve or -ve.py
@@ -0,0 +1,11 @@
+#python code goes here
+#python version :3 
+a=int(input())
+if(a>0):
+ print("a is positive")
+elif(a==0):
+ print("a is zero")
+elif(a<0):
+ print("a is negative")
+else:
+  print("a is invalid")
