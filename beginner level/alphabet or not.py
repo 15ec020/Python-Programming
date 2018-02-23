08f557078d1691807226e6d63762643e8076eed3
15ec020 <karshinisekar1998@gmail.com>
2018-02-23 10:54:09 +0530

    Create alphabet or not.py

---
+++ beginner level/alphabet or not.py
@@ -0,0 +1,9 @@
+#python code goes here
+#python version :3 
+a=raw_input()
+if (a>='a' and a<='z'):
+ print("an alphabet")
+elif(a>='A' and a<='Z'):
+ print("an alphabet")
+else:
+ print("not an alphabet")
