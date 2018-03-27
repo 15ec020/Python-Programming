c46189f53806848a9c25bd97fd4b447af19da16c
15ec020 <karshinisekar1998@gmail.com>
2018-03-27 15:55:29 +0530

    Create factorial1.py

---
+++ beginner level/factorial1.py
@@ -0,0 +1,9 @@
+num=int(input())
+factorial=1
+if(num>=20):
+ print("factorial value does not exist")
+else:
+ for i in range(1,num+1):
+  factorial=factorial*i
+  print("the value is",factorial)
+    
