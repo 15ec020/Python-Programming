0efddaee3d24fa703111cffa1e21650c7ec8dfd6
15ec020 <karshinisekar1998@gmail.com>
2018-07-24 12:17:07 +0530

    Create alphabetsandnumbers.c

---
+++ beginner level/alphabetsandnumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+   char a;
+   scanf("%s",a);
+   if(a=(('a'<='z')&&(1<=9)))
+   {
+       printf("yes");
+   }
+   else
+   {
+     printf("no");
+   }
+}
